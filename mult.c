22d3acbbeab0ea1ac43e0401ca1c7ab286326a33
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2017-09-21 14:37:15 +0800

    Test 1

--- mult.c
+++ mult.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-	for(int i=1; i<=81; i++) {
-		printf("%d*%d=%2d, ", i%9==0?i/9:i/9+1, i%9==0?9:i%9, (i%9==0?i/9:i/9+1)*(i%9==0?9:i%9));
-		if(i%9==0) puts("");
+	for(int i=0; i<81; i++) {
+		printf("%d*%d=%2d, ", i/9+1, i%9+1, (i/9+1)*(i%9+1));
+		if(i%9+1==9) puts("");
 	}
 	return 0;
 }
